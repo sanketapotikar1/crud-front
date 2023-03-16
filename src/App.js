b9a6593a8cdb1fb3aa0a0f611ab1382e6f36927f
Sanket Apotikar <sanketapotikar1@gmail.com>
2023-03-16 11:15:29 +0530

    "crud-front"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbaar from "./components/Navbaar";
+import Home from './components/Home.js';
+import Register from './components/Register';
+import Edit from './components/Edit';
+import Error from './components/Error';
+import Details from './components/Details';
+import {Route, Routes} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbaar />
+      <Routes>
+      <Route  path="/" element={<Home/>} />
+      <Route  path="/register" element={<Register/>} />
+      <Route  path="/edit/:id" element={<Edit/>} />
+      <Route  path="/getuser/:id" element={<Details/>} />
+      <Route path="*" element={<Error />} />
+    </Routes>
     </div>
   );
 }
